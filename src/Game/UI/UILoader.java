43d8205dc056b1336e57503101992bcf346d6800
Karl Damus <getaholdofkarl@gmail.com>
2021-05-14 23:48:52 -0400

    Create UILoader.java

---
+++ src/Game/UI/UILoader.java
@@ -0,0 +1,19 @@
+package Game.UI;
+
+import Players.Player;
+
+import javax.swing.*;
+
+public class UILoader {
+    public static void main(String[] args) {
+        JFrame GAME_WINDOW = new JFrame("Stardew Valley");
+    }
+
+    public static void mainMenu() {
+
+    }
+
+    public static void playerSetup(Player player) {
+
+    }
+}
